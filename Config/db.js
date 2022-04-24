e9ac0eb31589b2694df8a5f1a8b06f5703706622
Alyxander Byfield <Kandachi500@gmail.com>
2022-04-24 17:35:08 -0400

    Initial commit

---
+++ Config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SessionSecret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://127.0.0.1/contacts";
+exports.RemoteURI = "mongodb+srv://zanyder:MNHxEKtfptZvOEVC@cluster0.rptel.mongodb.net/contacts?retryWrites=true&w=majority";
+exports.HostName = "localhost";
+exports.SessionSecret = "WEBD6201SessionSecret";
+//# sourceMappingURL=db.js.map
